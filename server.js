897acc2d90361cbcde793842aae366296b2e32b1
Kale Renuka INPU <Renuka.Kale@syngenta.com>
2024-04-15 12:08:58 +0530

    Commiting

--- server.js
+++ server.js
@@ -57,28 +57,28 @@ app.listen(3000, () => {
 //   );
 // };
 
-const express = require('express');
-const axios = require('axios');
-const app = express();
+// const express = require('express');
+// const axios = require('axios');
+// const app = express();
 
-const token = 'eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJjcm9wd2lzZUpXVCIsImF1ZCI6IjkxNzg3NTczNzY0OUlOIiwic3ViIjoiYzBlNGE2NzAtODMwZS00MzgwLTg0ZTktNDVmNDk1YWViNWFhIiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9VU0VSIl0sImNvdW50cnlDb2RlIjoiSU4iLCJpYXQiOjE2Nzg3NzI1Mjl9.DMsJbMC5S0WKfP9Xl78TLmz5oUylSFsFnlndnXCTKk0QLDGbb-pOPNs7a1OZziHVlPp_W2-xtRi_Wuf3LhnyYg';
+// const token = 'eyJhbGciOiJIUzUxMiJ9.eyJqdGkiOiJjcm9wd2lzZUpXVCIsImF1ZCI6IjkxNzg3NTczNzY0OUlOIiwic3ViIjoiYzBlNGE2NzAtODMwZS00MzgwLTg0ZTktNDVmNDk1YWViNWFhIiwiYXV0aG9yaXRpZXMiOlsiUk9MRV9VU0VSIl0sImNvdW50cnlDb2RlIjoiSU4iLCJpYXQiOjE2Nzg3NzI1Mjl9.DMsJbMC5S0WKfP9Xl78TLmz5oUylSFsFnlndnXCTKk0QLDGbb-pOPNs7a1OZziHVlPp_W2-xtRi_Wuf3LhnyYg';
 
-app.get('/fetchData', async (req, res) => {
-  try {
-    const response = await axios.get('https://qa-in.api.cropwise-smallgrowers.syndpe.com/api/v1/services', {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    res.json(response.data);
-  } catch (error) {
-    res.status(500).json({ error: 'Error fetching data from the external API' });
-  }
-});
+// app.get('/fetchData', async (req, res) => {
+//   try {
+//     const response = await axios.get('https://qa-in.api.cropwise-smallgrowers.syndpe.com/api/v1/services', {
+//       headers: {
+//         Authorization: `Bearer ${token}`,
+//       },
+//     });
+//     res.json(response.data);
+//   } catch (error) {
+//     res.status(500).json({ error: 'Error fetching data from the external API' });
+//   }
+// });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+// app.listen(3000, () => {
+//   console.log('Server is running on port 3000');
+// });
 
 
 
