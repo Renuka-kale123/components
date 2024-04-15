a6484ed70c0de55c5827a3806db6812a3443539a
Renuka-Kale111 <Renuka.Kale@syngenta.com>
2024-04-15 11:55:40 +0530

    Add files via upload

---
+++ Task/ButtonComponent.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Text, View, ViewStyle, TextStyle, TouchableOpacity, TouchableOpacityProps } from 'react-native';
+ 
+interface ButtonProps extends TouchableOpacityProps {
+  width: number ;
+  height: number ;
+  bgColor: string;
+  title: string;
+  titleColor: string;
+  titleSize: number;
+  borderRadius: number;
+  accessibiltyLabel?:string;
+  accesible?:boolean;
+  extraParam?:string;
+  onPress?():void;
+}
+ 
+const ButtonComponent = ({
+  width,
+  height,
+  bgColor,
+  title,
+  titleColor,
+  titleSize,
+  borderRadius,
+  onPress,
+  accessibilityLabel,
+  accessible,
+  extraParam,
+  ...rest
+}: ButtonProps) => {
+  return (
+    <TouchableOpacity
+     onPress={onPress}  
+     accessibilityLabel={accessibilityLabel}
+    accessible={accessible}
+    {...rest}>
+<View style={{
+        width: width,
+        height: height,
+        backgroundColor: bgColor,
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'center',
+        borderRadius: borderRadius,
+        // margin:margin,
+        // padding:padding
+    }}>
+<Text style={{ color: titleColor, fontSize: titleSize }}>{title}</Text>
+</View>
+</TouchableOpacity>
+  );
+};
+ 
+export default ButtonComponent;
