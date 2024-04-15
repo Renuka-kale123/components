94be4ac9440f793e1dce27e5b328035f1ee6a4f0
Kale Renuka INPU <Renuka.Kale@syngenta.com>
2024-04-15 12:41:17 +0530

    commiting index.d.ts

---
+++ insex.d.ts
@@ -0,0 +1,25 @@
+// index.d.ts
+import { TouchableOpacityProps } from 'react-native';
+
+declare module "pkg" {
+  interface ButtonProps extends TouchableOpacityProps {
+    width: number;
+    height: number;
+    bgColor: string;
+    title: string;
+    titleColor: string;
+    titleSize: number;
+    borderRadius: number;
+    accessibiltyLabel?: string;
+    accessible?: boolean;
+    extraParam?: string;
+    onPress?(): void;
+  }
+
+  export const ButtonComponent: React.ComponentType<ButtonProps>;
+
+  export function testRenderSnapshot(): void;
+  export function testClickFunction(): void;
+  export function testButtonStyles(): void;
+  export function testButtonAccessibility(): void;
+}
