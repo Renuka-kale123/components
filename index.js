6e1e8e85d7adaad52966d604d32fd6dc59bdb6b7
Kale Renuka INPU <Renuka.Kale@syngenta.com>
2024-04-17 11:55:36 +0530

    exporting buttonComponent

--- index.js
+++ index.js
@@ -2,8 +2,17 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { ButtonComponent, testRenderSnapshot, testClickFunction, testButtonStyles, testButtonAccessibility } from 'pkg';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
-export { default as CustomButton } from './App';
+import { name as appName } from './app.json';
+
+// Use the ButtonComponent or call the test functions as needed
+// For example:
+testRenderSnapshot();
+testClickFunction();
+testButtonStyles();
+testButtonAccessibility();
+
+// Register the main component of your app
 AppRegistry.registerComponent(appName, () => App);
