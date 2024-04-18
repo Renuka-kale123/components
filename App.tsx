ec5ea2daa543d5f3f838c91ff1374ad115a05542
Kale Renuka INPU <Renuka.Kale@syngenta.com>
2024-04-18 15:49:13 +0530

    commiting

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, StyleSheet, Dimensions,Text } from 'react-native';
-import ButtonComponent from './Task/ButtonComponent';
+import {ButtonComponent} from 'pkg';
 const { width, height } = Dimensions.get('screen');
  
 const App = () => {
@@ -9,7 +9,7 @@ const App = () => {
   }
   return (
 <View style={styles.container}>
-<ButtonComponent width={200} height={50} bgColor={'orange'} title={'my button'} titleColor={'black'} titleSize={20} borderRadius={10} onPress={handleClick}/>
+ <ButtonComponent width={200} height={50} bgColor={'orange'} title={'my button'} titleColor={'black'} titleSize={20} borderRadius={10} onPress={handleClick}/> 
 </View>
   );
 };
