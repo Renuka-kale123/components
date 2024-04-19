d25ecf54cee5ea7b15cfb8a022196d942155f165
Kale Renuka INPU <Renuka.Kale@syngenta.com>
2024-04-19 16:39:03 +0530

    removing app

--- App.tsx
+++ App.tsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import { View, StyleSheet, Dimensions,Text } from 'react-native';
-import {ButtonComponent} from 'pkg';
-const { width, height } = Dimensions.get('screen');
+// import React from 'react';
+// import { View, StyleSheet, Dimensions,Text } from 'react-native';
+// import {ButtonComponent} from 'pkg';
+// const { width, height } = Dimensions.get('screen');
  
-const App = () => {
-  const handleClick=()=>{
-    console.warn("Button Clicked")
-  }
-  return (
-<View style={styles.container}>
- <ButtonComponent width={200} height={50} bgColor={'orange'} title={'my button'} titleColor={'black'} titleSize={20} borderRadius={10} onPress={handleClick}/> 
-</View>
-  );
-};
+// const App = () => {
+//   const handleClick=()=>{
+//     console.warn("Button Clicked")
+//   }
+//   return (
+// <View style={styles.container}>
+//  <ButtonComponent width={200} height={50} bgColor={'orange'} title={'my button'} titleColor={'black'} titleSize={20} borderRadius={10} onPress={handleClick}/> 
+// </View>
+//   );
+// };
  
-const styles = StyleSheet.create({
-  container: {
-   flex: 1,
-   width:width,
-   height:height,
-   justifyContent:'center',
-   alignItems:'center'
-  }
-});
+// const styles = StyleSheet.create({
+//   container: {
+//    flex: 1,
+//    width:width,
+//    height:height,
+//    justifyContent:'center',
+//    alignItems:'center'
+//   }
+// });
  
-export default App
+// export default App
