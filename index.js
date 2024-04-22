1fd5b3f824626303339f8de51a15900816fff039
Kale Renuka INPU <Renuka.Kale@syngenta.com>
2024-04-22 16:16:53 +0530

    commiting

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
+import React from 'react';
+import { TouchableOpacity, Text } from 'react-native';
+import renderer from 'react-test-renderer';
+import { fireEvent } from '@testing-library/react-native';
+
 export const testButtonAccessibility=()=>{
-  console.log("Hello")
+    const component = renderer.create(
+<ButtonComponent
+      width={200}
+      height={50}
+      bgColor={'orange'}
+      title={'my button'}
+      titleColor={'black'}
+      titleSize={20}
+      borderRadius={10}
+      accessibilityLabel="My button"
+      accessible={true}
+    />
+  );
+  const buttonInstance = component.root.findByType(TouchableOpacity); // Change ButtonComponent to TouchableOpacity
+ 
+  expect(buttonInstance.props.accessibilityLabel).toBe('My button');
+  expect(buttonInstance.props.accessible).toBe(true);
 }
 export function testClickFunction() {
   console.log("Hello22222")
