36eee8cf2fb2d6336efe7c84e3c4dc8513e9f2df
Kale Renuka INPU <Renuka.Kale@syngenta.com>
2024-04-22 11:51:20 +0530

    second commit

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,20 @@
+import { TouchableOpacityProps } from 'react-native';
 export function testButtonAccessibility(): any;
+interface ButtonProps extends TouchableOpacityProps {
+    width: number;
+    height: number;
+    bgColor: string;
+    title: string;
+    titleColor: string;
+    titleSize: number;
+    borderRadius: number;
+    accessibilityLabel?: string; // Corrected spelling
+    accessible?: boolean;
+    extraParam?: string;
+    onPress?(): void;
+  }
+
+export const ButtonComponent: React.ComponentType<ButtonProps>; 
+export function testRenderSnapshot(): void;
+export function testClickFunction(): void;
+export function testButtonStyles(): void;
--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 export const testButtonAccessibility=()=>{
   console.log("Hello")
 }
+
+export const ButtonComponent = ({
+  width,
+  height,
+  bgColor,
+  title,
+  titleColor,
+  titleSize,
+  borderRadius,
+  accessibilityLabel,
+  accessible,
+  onPress,
+}) => {
+  return (
+<TouchableOpacity
+      style={{
+        width,
+        height,
+        backgroundColor: bgColor,
+        borderRadius,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+      accessibilityLabel={accessibilityLabel}
+      accessible={accessible}
+      onPress={onPress}
+>
+<Text style={{ color: titleColor, fontSize: titleSize }}>{title}</Text>
+</TouchableOpacity>
+  );
+};
