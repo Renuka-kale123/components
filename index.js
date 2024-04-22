72e0ebe8ed4f60f68999f67700cd13c8a922c776
Kale Renuka INPU <Renuka.Kale@syngenta.com>
2024-04-22 11:56:34 +0530

    commiting 3

--- index.js
+++ index.js
@@ -1,7 +1,54 @@
 export const testButtonAccessibility=()=>{
   console.log("Hello")
 }
-
+export function testClickFunction() {
+  console.log("Hello22222")
+  let clicked = false;
+  const handleClick = () => {
+    clicked = true;
+  };
+ 
+  const component = renderer.create(
+<ButtonComponent
+      width={200}
+      height={50}
+      bgColor={'orange'}
+      title={'my button'}
+      titleColor={'black'}
+      titleSize={20}
+      borderRadius={10}
+      onPress={handleClick}
+    />
+  );
+ 
+  const buttonInstance = component.root.findByType(TouchableOpacity); // Change ButtonComponent to TouchableOpacity
+  fireEvent.press(buttonInstance);
+ 
+  expect(clicked).toBe(true);
+}
+ 
+export function testButtonStyles() {
+  const component = renderer.create(
+<ButtonComponent
+      width={200}
+      height={50}
+      bgColor={'orange'}
+      title={'my button'}
+      titleColor={'black'}
+      titleSize={20}
+      borderRadius={10}
+    />
+  );
+  const buttonInstance = component.root.findByType(TouchableOpacity); // Change ButtonComponent to TouchableOpacity
+ 
+  expect(buttonInstance.props.style.width).toBe(200);
+  expect(buttonInstance.props.style.height).toBe(50);
+  expect(buttonInstance.props.style.backgroundColor).toBe('orange');
+  expect(buttonInstance.findByType(Text).props.style.color).toBe('black');
+  expect(buttonInstance.findByType(Text).props.style.fontSize).toBe(20);
+  expect(buttonInstance.props.style.borderRadius).toBe(10);
+}
+ 
 export const ButtonComponent = ({
   width,
   height,
