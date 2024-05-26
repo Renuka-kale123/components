ccfd73fc4e2937556c34b93d00e35d6c89adba1f
Renuka-Kale111 <Renuka.Kale@syngenta.com>
2024-05-26 10:13:30 +0530

    Add files via upload

---
+++ src/Theme/TextStyles.ts
@@ -0,0 +1,222 @@
+import { StyleSheet, TextStyle } from 'react-native';
+import { fonts, sizes } from './Fonts';
+
+export const textStyles = StyleSheet.create<Record<string, TextStyle>>({
+  dmsansRegular2224: {
+    fontFamily: fonts.dmsans_regular,
+    fontSize: sizes.size22,
+    lineHeight: sizes.size24
+  },
+  dmsansItalic2224: {
+    fontFamily: fonts.dmsans_italic,
+    fontSize: sizes.size22,
+    lineHeight: sizes.size24
+  },
+  dmsansMedium2224: {
+    fontFamily: fonts.dmsans_medium,
+    fontSize: sizes.size22,
+    lineHeight: sizes.size24
+  },
+  dmsansMediumItalic2224: {
+    fontFamily: fonts.dmsans_medium_italic,
+    fontSize: sizes.size22,
+    lineHeight: sizes.size24
+  },
+  dmsansBold2224: {
+    fontFamily: fonts.dmsans_bold,
+    fontSize: sizes.size22,
+    lineHeight: sizes.size24
+  },
+  dmsansBoldItalic2224: {
+    fontFamily: fonts.dmsans_bold_italic,
+    fontSize: sizes.size22,
+    lineHeight: sizes.size24
+  },
+  dmsansRegular1622: {
+    fontFamily: fonts.dmsans_regular,
+    fontSize: sizes.size16,
+    lineHeight: sizes.size22
+  },
+  dmsansBoldItalic2620: {
+    fontFamily: fonts.dmsans_bold,
+    fontSize: sizes.size26,
+    lineHeight: sizes.size20
+  },
+  dmsansRegular1624: {
+    fontFamily: fonts.dmsans_regular,
+    fontSize: sizes.size16,
+    lineHeight: sizes.size24,
+    fontWeight: '400'
+  },
+  dmsansRegular2222: {
+    fontFamily: fonts.dmsans_regular,
+    fontSize: sizes.size22,
+    lineHeight: sizes.size22,
+    fontWeight: '700'
+  },
+  jannaLTRegular1420: {
+    fontSize: sizes.size14,
+    fontWeight: 'bold',
+    fontStyle: 'normal',
+    lineHeight: sizes.size20,
+    fontFamily: 'Janna LT'
+  },
+  dmsansBold1824: {
+    fontFamily: fonts.dmsans_bold,
+    fontSize: sizes.size18,
+    lineHeight: sizes.size24
+  },
+  dmsansBold1822: {
+    fontFamily: fonts.dmsans_bold,
+    fontSize: sizes.size18,
+    lineHeight: sizes.size22
+  },
+  dmsansBold1624: {
+    fontFamily: fonts.dmsans_bold,
+    fontSize: sizes.size16,
+    lineHeight: sizes.size24
+  },
+  dmsansRegular1420: {
+    fontFamily: fonts.dmsans_regular,
+    fontSize: sizes.size14,
+    lineHeight: sizes.size20
+  },
+  dmsansRegular1422: {
+    fontFamily: fonts.dmsans_regular,
+    fontSize: sizes.size14,
+    lineHeight: sizes.size22
+  },
+  dmsansRegular1424: {
+    fontFamily: fonts.dmsans_regular,
+    fontSize: sizes.size14,
+    lineHeight: sizes.size24
+  },
+  dmsansBold1520: {
+    fontFamily: fonts.dmsans_bold,
+    fontSize: sizes.size15,
+    lineHeight: sizes.size20
+  },
+  dmsansRegular1216: {
+    fontFamily: fonts.dmsans_regular,
+    fontSize: sizes.size12,
+    lineHeight: sizes.size16
+  },
+  dmsansBold1820: {
+    fontFamily: fonts.dmsans_bold,
+    fontSize: sizes.size18,
+    lineHeight: sizes.size20
+  },
+  dmsansBold1826: {
+    fontFamily: fonts.dmsans_bold,
+    fontSize: sizes.size18,
+    lineHeight: sizes.size26
+  },
+  dmsansBold1620: {
+    fontFamily: fonts.dmsans_bold,
+    fontSize: sizes.size16,
+    lineHeight: sizes.size20
+  },
+  dmsansRegular1620: {
+    fontFamily: fonts.dmsans_regular,
+    fontSize: sizes.size16,
+    lineHeight: sizes.size20
+  },
+  dmsansBold1418: {
+    fontFamily: fonts.dmsans_bold,
+    fontSize: sizes.size14,
+    lineHeight: sizes.size18
+  },
+  dmsansBold1218: {
+    fontFamily: fonts.dmsans_regular,
+    fontSize: sizes.size12,
+    lineHeight: sizes.size18,
+    fontWeight: '400'
+  },
+  dmsansRegular1118: {
+    fontFamily: fonts.dmsans_regular,
+    fontSize: sizes.size11,
+    lineHeight: sizes.size18,
+    fontWeight: '700'
+  },
+  dmsansRegular2028: {
+    fontFamily: fonts.dmsans_regular,
+    fontSize: sizes.size20,
+    lineHeight: sizes.size28,
+    fontWeight: '700'
+  },
+  dmsansRegular1320: {
+    fontFamily: fonts.dmsans_regular,
+    fontSize: sizes.size13,
+    lineHeight: sizes.size20,
+    fontWeight: '700'
+  },
+  dmsansRegular16247: {
+    fontFamily: fonts.dmsans_regular,
+    fontSize: sizes.size16,
+    lineHeight: sizes.size24,
+    fontWeight: '700'
+  },
+  dmsansRegular13164: {
+    fontFamily: fonts.dmsans_regular,
+    fontSize: sizes.size13,
+    lineHeight: sizes.size16,
+    fontWeight: '400'
+  },
+  dmsansBold1216: {
+    fontFamily: fonts.dmsans_bold,
+    fontSize: sizes.size12,
+    lineHeight: sizes.size16
+  },
+  dmsansMedium1624: {
+    fontFamily: fonts.dmsans_medium,
+    fontSize: sizes.size16,
+    lineHeight: sizes.size24
+  },
+  dmsansMedium1216: {
+    fontFamily: fonts.dmsans_medium,
+    fontSize: sizes.size12,
+    lineHeight: sizes.size16
+  },
+  dmsansRegular1218: {
+    fontFamily: fonts.dmsans_regular,
+    fontSize: sizes.size12,
+    lineHeight: sizes.size18
+  },
+  dmsansRegular1224: {
+    fontFamily: fonts.dmsans_regular,
+    fontSize: sizes.size12,
+    lineHeight: sizes.size24
+  },
+  dmsansRegular1220: {
+    fontFamily: fonts.dmsans_regular,
+    fontSize: sizes.size12,
+    lineHeight: sizes.size20,
+    fontWeight: '400'
+  },
+  dmsansBold3448: {
+    fontFamily: fonts.dmsans_bold,
+    fontSize: sizes.size34,
+    lineHeight: sizes.size48
+  },
+  dmsansRegular1418: {
+    fontFamily: fonts.dmsans_regular,
+    fontSize: sizes.size14,
+    lineHeight: sizes.size18
+  },
+  dmsansMedium1422: {
+    fontFamily: fonts.dmsans_medium,
+    fontSize: sizes.size14,
+    lineHeight: sizes.size22
+  },
+  dmsansRegular1316: {
+    fontFamily: fonts.dmsans_regular,
+    fontSize: sizes.size13,
+    lineHeight: sizes.size16,
+    fontWeight: '400'
+  },
+  dmsansBold1220: {
+    fontFamily: fonts.dmsans_bold,
+    fontSize: sizes.size12,
+    lineHeight: sizes.size20
+  }
+});
